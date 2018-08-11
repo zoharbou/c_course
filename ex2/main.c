b8804a1aa4e20ad7f78828009b43af84bb6d0b0e
zoharbou <zoharbou@gmail.com>
2018-08-11 20:39:30 +0300

    init board and random location

--- ex2/main.c
+++ ex2/main.c
@@ -1,29 +1,197 @@
 #include <stdio.h>
+#include <mem.h>
+#include <stdlib.h>
 
- char board[10][10] = {
-        '0','0','0','0','0','x','0','0','0','0',
-        '0','0','0','0','0','x','0','0','0','0',
-        '0','0','0','0','0','x','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0',
-        '0','0','0','0','0','0','0','0','0','0'
+const char HIT = 'x';
+const char MISS = 'o';
+const char INIT_CELL = '_';
+const char EMPTY = '0';
+const int sizeOfBoard = 10;
+const int TRUE = 0;
+const int FALSE = 1;
+
+
+typedef struct Direction {
+    int addToRow;
+    int addToColumn;
+} Direction;
+
+Direction directions[4] = {
+        {0,  1},
+        {0,  -1},
+        {1,  0},
+        {-1, 0}
 };
 
-void printBoard(char board[][10], int sizeOfBoard) {
+typedef struct Cell {
+    char content;
+    char status;
+} Cell;
+
+
+Cell board[10][10];
+
+
+void initBoard() {
+    int i, j;
+    for (i = 0; i < sizeOfBoard; ++i) {
+        for (j = 0; j < sizeOfBoard; ++j) {
+            board[i][j].status = INIT_CELL;
+        }
+    }
+}
+
+void initShipBoard() {
+    int i, j;
+    for (i = 0; i < sizeOfBoard; ++i) {
+        for (j = 0; j < sizeOfBoard; ++j) {
+            board[i][j].content = EMPTY;
+        }
+    }
+}
+
+int isCellFree(const int row, const int column) {
+    if (board[row][column].content == EMPTY) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int isIndexInBoard(const int row, const int col) {
+    if (row >= 0 && row < sizeOfBoard && col >= 0 && col < sizeOfBoard) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+void getRandLocation(int *row, int *column, Direction *direction, int sizeOfShip) {
+    int rowStart, colStart;
+    do { // get the start location for the ship randomly
+        rowStart = rand() % sizeOfBoard;
+        colStart = rand() % sizeOfBoard;
+    } while (isCellFree(rowStart, colStart) == FALSE);
+
+    int rowFinish, colFinish;
+    Direction curDirection;
+    do { // get the direction for the ship to stand randomly
+        curDirection = directions[rand() % 4]; // get a random direction
+        rowFinish = rowStart + curDirection.addToRow * sizeOfShip;
+        colFinish = colStart + curDirection.addToColumn * sizeOfShip;
+    } while (isIndexInBoard(rowFinish, colFinish) == FALSE);
+    // init the final values
+    *row = rowStart;
+    *column = colStart;
+    *direction = curDirection;
+}
+
+int checkCollision(int row, int col, const Direction direction, int sizeOfShip) {
+    int i;
+    for (i = 0; i < sizeOfShip; ++i) {
+        if (isCellFree(row, col) == FALSE) {
+            return FALSE;
+        }
+        row += direction.addToRow;
+        col += direction.addToColumn;
+    }
+    return TRUE;
+}
+
+
+void placeShip(int sizeOfShip, char shipSign) {
+    int row, col, i;
+    Direction direction;
+    do {
+        getRandLocation(&row, &col, &direction, sizeOfShip); // find a location and direction
+    } while (checkCollision(row, col, direction, sizeOfShip) == FALSE); // check if the place is free
+
+    for (i = 0; i < sizeOfShip; ++i) { // now place the ship
+        board[row][col].content = shipSign;
+        row += direction.addToRow;
+        col += direction.addToColumn;
+    }
+}
+
+int isLetter(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+//TO.DO fix this!! its not working
+void getMove(int *row, int *column) {
+    printf("please make your move:\n");
+    char rowInChar;
+    int currCol;
+    do {
+        if (scanf("%c%d", &rowInChar, &currCol) == 2 && isLetter(rowInChar) == TRUE) {
+            *row = rowInChar - 'a' + 1;
+            *column = currCol;
+            break;
+        } else {
+            fprintf(stderr, "Invalid move, try again\n");
+        }
+    } while (1);
+}
+
+void placeMove(const int row, const int column) {
+    if (board[row][column].status != INIT_CELL) {
+        printf("you already placed that move\n");
+        return;
+    }
+    if (board[row][column].content != EMPTY) { // there is a ship part in this cell
+        board[row][column].status = HIT;
+        printf("Hit!\n");
+        return;
+    }
+    board[row][column].status = MISS; // the cell is empty
+    printf("Miss :(\n");
+}
+
+
+void printBoard(Cell board[][10], int sizeOfBoard) {
     int rowIndex, colIndex;
     for (rowIndex = 0; rowIndex < sizeOfBoard; ++rowIndex) {
         for (colIndex = 0; colIndex < sizeOfBoard; ++colIndex) {
-            printf(" %c", board[rowIndex][colIndex]);
+            printf(" %c", board[rowIndex][colIndex].status);
         }
         printf("\n");
     }
 }
 
+// TO.DO delete before final submmision!!!!!!!
+void printBehind(Cell board[][10], int sizeOfBoard) {
+    int rowIndex, colIndex;
+    for (rowIndex = 0; rowIndex < sizeOfBoard; ++rowIndex) {
+        for (colIndex = 0; colIndex < sizeOfBoard; ++colIndex) {
+            printf(" %c", board[rowIndex][colIndex].content);
+        }
+        printf("\n");
+    }
+}
+
+
 int main() {
-    printBoard(board, 10);
+
+    initShipBoard();
+    placeShip(5, 'a');
+    placeShip(4, 'b');
+    placeShip(3, 'c');
+    placeShip(3, 'd');
+    placeShip(2, 'e');
+
+    printBehind(board, sizeOfBoard);
+
+//    initBoard();
+//    printBoard(board, 10);
+//    int currRow;
+//    int currColumn;
+//
+//    getMove(&currRow, &currColumn);
+//    placeMove(currRow, currColumn);
+//    printBoard(board, 10);
+//    getMove(&currRow, &currColumn);
+//    placeMove(currRow, currColumn);
+//    printBoard(board, 10);
     return 0;
 }
