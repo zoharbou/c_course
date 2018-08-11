1741f02b3eaa9bfebaf4f0f4e7581f41e398dcce
zoharbou <zoharbou@gmail.com>
2018-08-11 15:52:46 +0300

    print board method.

--- ex2/main.c
+++ ex2/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+ char board[10][10] = {
+        '0','0','0','0','0','x','0','0','0','0',
+        '0','0','0','0','0','x','0','0','0','0',
+        '0','0','0','0','0','x','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0',
+        '0','0','0','0','0','0','0','0','0','0'
+};
+
+void printBoard(char board[][10], int sizeOfBoard) {
+    int rowIndex, colIndex;
+    for (rowIndex = 0; rowIndex < sizeOfBoard; ++rowIndex) {
+        for (colIndex = 0; colIndex < sizeOfBoard; ++colIndex) {
+            printf(" %c", board[rowIndex][colIndex]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
-    printf("Hello, matan!\n");
+    printBoard(board, 10);
     return 0;
 }
